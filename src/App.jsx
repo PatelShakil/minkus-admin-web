8d1c9dc6df2d170fb4979889bdbc694f2e090c65
PatelShakil <patelsakib95@gmail.com>
2024-09-20 23:54:47 +0530

    Forms Completed

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,12 @@ import Cookies from "js-cookie";
 import ScheduleEventPage from "./pages/ScheduleEventPage.jsx";
 import ViewTask from "./pages/ViewTask.jsx";
 import FormA from "./pages/forms/FormA.jsx";
+import FormB from "./pages/forms/FormB.jsx";
+import FormC from "./pages/forms/FormC.jsx";
+import FormD from "./pages/forms/FormD.jsx";
+import FormE from "./pages/forms/FormE.jsx";
+import FormF from "./pages/forms/FormF.jsx";
+import FormG from "./pages/forms/FormG.jsx";
 
 function App() {
 
@@ -25,6 +31,12 @@ function App() {
           <Route path={Pages.ScheduleEvent} element={<ScheduleEventPage />} />
           <Route path={Pages.ViewTasks} element={<ViewTask />} />
           <Route path={Pages.FormA} element={<FormA />} />
+          <Route path={Pages.FormB} element={<FormB />} />
+          <Route path={Pages.FormC} element={<FormC />} />
+          <Route path={Pages.FormD} element={<FormD />} />
+          <Route path={Pages.FormE} element={<FormE />} />
+          <Route path={Pages.FormF} element={<FormF />} />
+          <Route path={Pages.FormG} element={<FormG />} />
       </Routes>
 
   )
---
+++ src/pages/forms/FormD.jsx
@@ -0,0 +1,56 @@
+import useFetchFormsState from "../../components/states/FetchFormsState.jsx";
+import Loading from "../../components/Loading.jsx";
+import getJa from "../../utils/getJa.jsx";
+
+const FormD = () =>{
+
+    const {isLoading,data,error} = useFetchFormsState(4);
+
+    return <div className={"flex flex-col"}>
+        {
+            isLoading && (
+                <Loading/>
+            )
+        }
+
+        {
+            error && (
+                <div className={"flex mt-20"}>
+                    <p className={"text-red-400"}>{error}</p>
+                </div>
+            )
+        }
+        <div className={"fixed top-0 w-full text-center p-3 bg-gray-300 z-10 shadow-2xl"}>
+            <p>Hinterhof (wenn vorhanden)</p>
+        </div>
+        <div className={"flex mt-14 flex-col mx-2"}>
+            {
+                data && (
+                    data.map((form, index) => (<div key={index} className={"flex shadow-sm p-2 m-1 rounded-md bg-gray-100"}>
+                        <span className={"text-sm grid grid-cols-1 lg:grid-cols-2 w-full mx-2 justify-center"}>
+                            <div className={"col-span-1"}>
+                            <p>Name :<b>{form.name}</b></p>
+                            <p>Rundgang Datum : <b>{form.rundgang}</b></p>
+                            <p>Objekt / Straße : <b>{form.state}</b></p>
+                            <p>Sauberkeit : <b>{form.sauberkit}</b></p>
+                            <p>Unratbeseitigung (falls im Vertrag)  : <b>{getJa(form.checkbox1)}</b></p>
+                            </div>
+                            <div className={"col-span-1 justify-center flex flex-col"}>
+                                <p>Email : <b>{form.user_email}</b></p>
+                            <div className={"shadow-sm rounded-md bg-white text-center"}>
+                                <img src={form.signature} alt={"Signature"} className={"w-96"} />
+                                <b>Signature</b>
+                            </div>
+                            </div>
+                        </span>
+
+                    </div>))
+                )
+            }
+
+        </div>
+
+    </div>
+}
+
+export default FormD
